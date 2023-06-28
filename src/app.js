0017af54fdddea768177ec6759911e679710cb7d
Goldor <goldorteps@gmail.com>
2023-06-28 19:10:23 +0200

    terminado sin hack

--- src/app.js
+++ src/app.js
@@ -5,7 +5,61 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateAllCombinations() {
+  let pronouns = ["I", "You", "He", "She", "We", "You", "They", "They"];
+  let adjectives = [
+    "good",
+    "bad",
+    "ugly",
+    "beautiful",
+    "crazy",
+    "smart",
+    "dumb",
+    "smart",
+    "dumb"
+  ];
+  let nouns = [
+    "dogcom",
+    "catnet",
+    "horse",
+    "birdedu",
+    "mouse",
+    "fish",
+    "rabbitcom",
+    "turtlebiz",
+    "hippoinfo"
+  ];
+  let extensions = [
+    ".com",
+    ".net",
+    ".org",
+    ".gov",
+    ".edu",
+    ".mil",
+    ".biz",
+    ".info",
+    ".pro"
+  ];
+
+  function allCombinations() {
+    let combinations = [];
+
+    for (let pronoun of pronouns) {
+      for (let adjective of adjectives) {
+        for (let noun of nouns) {
+          for (let extension of extensions) {
+            let domain = pronoun + adjective + noun + extension;
+            combinations.push(domain);
+          }
+        }
+      }
+    }
+
+    return combinations;
+  }
+
+  let allDomains = allCombinations();
+  document.getElementById("domainApp").innerHTML = allDomains;
+}
+
+generateAllCombinations();
